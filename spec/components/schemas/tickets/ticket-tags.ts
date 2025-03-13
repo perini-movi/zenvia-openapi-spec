50b7085250fbc3fe0e19a7fe331a54bcef88c8fb
Felipe Silvano Perini <felipe.perini@zenvia.com>
2025-03-13 10:41:28 -0300

    fix: return array of string

--- spec/components/schemas/tickets/ticket-tags.ts
+++ spec/components/schemas/tickets/ticket-tags.ts
@@ -4,10 +4,7 @@ import { createComponentRef } from '../../../../utils/ref';
 const base: SchemaObject = {
   title: 'Ticket tags',
   description: 'Ticket tags information.',
-  type: 'array',
-  items: {
-    type: 'string',
-  },
+  type: 'string',
 };
 
 export const ref = createComponentRef(__filename);
